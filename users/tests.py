fd9e72ff22108a1605470a71f748c0809c3b83bd
shinwaris <zoology.fb@gmail.com>
2022-09-29 22:17:58 +0500

    added docs

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,4 @@
+"""user app test layer"""
 from django.test import Client, TestCase
 
 # Create your tests here.
@@ -5,6 +6,7 @@ class TestUserApp(TestCase):
     """tests the user app"""
 
     def check_status(self):
+        """check the status of login page"""
         client = Client()
         response = client.get("users/login/")
 
