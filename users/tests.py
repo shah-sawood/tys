5cd4e215270ca05a105c6fc0d84bfbb34c4b4188
shinwaris <zoology.fb@gmail.com>
2022-09-29 22:13:58 +0500

    created login page status test case

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
 # Create your tests here.
+class TestUserApp(TestCase):
+    """tests the user app"""
+
+    def check_status(self):
+        client = Client()
+        response = client.get("users/login/")
+
+        self.assertEqual(response.status_code, 200)
